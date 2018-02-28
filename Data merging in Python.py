6fd3ce2d674c4d28b01227d47a02e39dcc82291a
YannickHogebrug <yannickhogebrug@live.nl>
2018-02-28 17:29:37 +0100

    Updated version

--- Data merging in Python.py
+++ Data merging in Python.py
@@ -9,14 +9,13 @@ This is a temporary script file.
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
-#Histogram of the winners
-data.plot(kind = "hist",y = "winning_Number")
-
-data["winning_Number"] = data["winning_Number"].astype("category")
+combats = pd.read_csv('C:/Users/Elitebook/Desktop/Machine Learning/Data/Original data/combats.csv')
+pokemon = pd.read_csv('C:/Users/Elitebook/Desktop/Machine Learning/Data/Original data/pokemon.csv')
 
 #Set the index of the pokemon table equal to the ID of the pokemon
-pokemon = pokemon.set_index('ID')
+pokemon = pokemon.set_index('#')
 
 #Add the stats of the winner behind his ID
 combats['winner_stats'] = [pokemon.loc[i] for i in combats['Winner']]
@@ -29,5 +28,27 @@ combats['Loser'] = [combats['First_pokemon'].iloc[i]
 
 #Add the stats of the loser behind his ID
 combats['loser_stats'] = [pokemon.loc[i] for i in combats['Loser']]
+
+#Comparing attack winner/loser
+attack_winner = [combats['winner_stats'].iloc[i].Attack for i in range(len(combats))]
+attack_loser = [combats['loser_stats'].iloc[i].Attack for i in range(len(combats))]
+
+plt.hist(x=attack_winner, color = 'green', alpha = 0.75, label = 'winner')
+plt.hist(x=attack_loser, color = 'red', alpha = 0.75, label = 'loser')
+plt.legend(loc = 'upper right')
+plt.xlabel('Attack value')
+plt.ylabel('Frequency')
+plt.show()
+
+#Comparing defense winner/loser
+defense_winner = [combats['winner_stats'].iloc[i].Defense for i in range(len(combats))]
+defense_loser = [combats['loser_stats'].iloc[i].Defense for i in range(len(combats))]
+
+plt.hist(x=defense_winner, color = 'green', alpha = 0.75, label = 'winner')
+plt.hist(x=defense_loser, color = 'red', alpha = 0.75, label = 'loser')
+plt.legend(loc = 'upper right')
+plt.xlabel('Defense value')
+plt.ylabel('Frequency')
+plt.show()
 
 meanSpeed = np.mean([combats['winner_stats'].iloc[i].Speed for i in range(len(combats))])
